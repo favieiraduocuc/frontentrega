76e11644da270c50d8ca4a3e70214fb5ac26f5ce
favieiraduocuc <fa.vieira@duocuc.cl>
2025-06-11 20:48:43 -0400

    Entrega

---
+++ proyecto_empleado_portal/js/data.js
@@ -0,0 +1,5 @@
+
+const envios = [
+  { codigo: "ABC123", estado: "Pendiente", origen: "Santiago", destino: "Valparaíso", receptor: "Francisca" },
+  { codigo: "XYZ789", estado: "En camino", origen: "Rancagua", destino: "Antofagasta", receptor: "Carlos López" }
+];
